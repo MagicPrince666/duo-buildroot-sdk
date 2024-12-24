0f7240d10e9c04c52cc1ef678f37d6bc7af79601
MagicPrince666 <846863428@qq.com>
2024-12-24 18:53:23 +0800

    添加软件包

---
+++ buildroot-2021.05/package/klipper/klipper3d-repl/klipper3d-repl.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# klipper3d-repl
+#
+################################################################################
+
+KLIPPER3D_REPL_VERSION = main
+KLIPPER3D_REPL_SITE = $(call github,unjordy,klipper-repl,$(KLIPPER3D_REPL_VERSION))
+KLIPPER3D_REPL_DEPENDENCIES = klipper3d python-prompt-toolkit python-pygments
+KLIPPER3D_REPL_SETUP_TYPE = setuptools
+
+define KLIPPER3D_REPL_BUILD_CMDS
+	cp $(KLIPPER3D_REPL_PKGDIR)/pyproject.toml $(@D)/pyproject.toml	
+	cp $(KLIPPER3D_REPL_PKGDIR)/setup.py $(@D)/setup.py
+endef
+
+$(eval $(python-package))
