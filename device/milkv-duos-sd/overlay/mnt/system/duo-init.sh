9911a74eb4c03e928d54f8030dea1d69383d04f4
MagicPrince666 <846863428@qq.com>
2025-01-16 18:56:26 +0800

    成功驱动spi屏幕

--- device/milkv-duos-sd/overlay/mnt/system/duo-init.sh
+++ device/milkv-duos-sd/overlay/mnt/system/duo-init.sh
@@ -3,6 +3,11 @@
 insmod /mnt/system/ko/gpio_backlight.ko
 insmod /mnt/system/ko/ads7846.ko
 insmod /mnt/system/ko/fb_ili9341.ko
+insmod /mnt/system/ko/aic8800_bsp.ko
+insmod /mnt/system/ko/aic8800_fdrv.ko
+insmod /mnt/system/ko/aic8800_btlpm.ko
+
+wpa_supplicant -i wlan0 -c /etc/wpa_supplicant.conf &
 
 # Set Pin-29(GP22) to GPIO
 # devmem 0x0502707c 32 0x111
